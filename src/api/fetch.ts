710635fffd5eb837986c04a26af35cd3d59172b3
fu013 <fu017@naver.com>
2024-01-31 13:23:06 +0900

    feat: SSR Post fetch add/fetch.ts refactoring

--- src/api/fetch.ts
+++ src/api/fetch.ts
@@ -4,28 +4,45 @@
 const baseURL = process.env.NEXT_PUBLIC_SERVER_URL as string;
 
 const SSGfetch: typeof fetch = async (endpoint) => {
-  const response = await fetch(baseURL + endpoint, { cache: "force-cache" });
-  return response.json();
+  const res = await fetch(baseURL + endpoint, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      // 'Authorization': 'Bearer YOUR_ACCESS_TOKEN',
+    },
+    cache: "force-cache",
+  });
+  return res;
 };
 
 /*
  * [SSR] no-store 옵션은 캐싱 작업을 하지 않고, 사용자가 요청할때마다, 데이터를 새롭게 불러오도록 설정한다.
  */
 const SSRfetch: typeof fetch = async (endpoint) => {
-  const response = await fetch(baseURL + endpoint, { cache: "no-store" });
-  return response.json();
+  const res = await fetch(baseURL + endpoint, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    cache: "no-store",
+  });
+  return res;
 };
 
 /*
  * [ISR] 설정한 시간마다 페이지를 새로 랜더링한다.
  */
 const ISRfetch: typeof fetch = async (endpoint) => {
-  const response = await fetch(baseURL + endpoint, {
+  const res = await fetch(baseURL + endpoint, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
     next: {
-      revalidate: 10, // Seconds
+      revalidate: 10,
     },
   });
-  return response.json();
+  return res;
 };
 
 export { SSGfetch, SSRfetch, ISRfetch };
--- src/component/Main.tsx
+++ src/component/Main.tsx
@@ -1,6 +1,21 @@
+import { SSRfetch } from "@/api/fetch";
 import Link from "next/link";
+import { useEffect } from "react";
 
-const Main = () => {
+interface iPost {
+  pk: number;
+  title: string;
+  content: string;
+  isPublic: string;
+  isReview: string;
+  comment: null | any;
+  updatedDate: string;
+  createdDate: string;
+}
+
+const Main = async () => {
+  const response = await SSRfetch("/post/all");
+  const jsonData: iPost[] = await response.json();
   return (
     <main className="basic-bg">
       <div className="feed-menu-wrapper basic-font-color">
@@ -11,26 +26,30 @@ const Main = () => {
           </li>
         </ol>
       </div>
-      <div className="feed-wrapper basic-font-color">
-        <div className="feed">
-          <div className="feed-photo"></div>
-          <h5 className="feed-title">Jenkins & Git 활용</h5>
-          <p className="feed-des basic-font-color">
-            📌 배경 최근 회사에 입사했는데, 20개 가까이되는 개발 프로젝트들이
-            AWS EC2 서비스로 호스팅
-          </p>
-          <ul className="tags">
-            <ol className="tag">ec2</ol>
-            <ol className="tag">git</ol>
-            <ol className="tag">jenkins</ol>
-          </ul>
-          <div className="feed-info">
-            <div>2024년 1월 30일</div>
-            <div>0개의 댓글</div>
-            <div>좋아요 4</div>
+      {/* Post Item Start */}
+      {jsonData?.map((item: iPost) => (
+        <div className="feed-wrapper basic-font-color" key={item.pk}>
+          <div className="feed">
+            <div className="feed-photo"></div>
+            <h5 className="feed-title">Jenkins & Git 활용</h5>
+            <p className="feed-des basic-font-color">
+              📌 배경 최근 회사에 입사했는데, 20개 가까이되는 개발 프로젝트들이
+              AWS EC2 서비스로 호스팅
+            </p>
+            <ul className="tags">
+              <ol className="tag">ec2</ol>
+              <ol className="tag">git</ol>
+              <ol className="tag">jenkins</ol>
+            </ul>
+            <div className="feed-info">
+              <div>2024년 1월 30일</div>
+              <div>0개의 댓글</div>
+              <div>좋아요 4</div>
+            </div>
           </div>
         </div>
-      </div>
+      ))}
+      {/* Post Item End */}
     </main>
   );
 };

a8427e5cedeb7d688c7c80eaa8a997bf2b9b5c62
fu013 <fu017@naver.com>
2024-02-01 15:54:12 +0900

    feat: setPost 예외처리

--- src/api/post.ts
+++ src/api/post.ts
@@ -5,12 +5,21 @@ const setPost = async (
   content: string,
   description: string,
   tags: string
-) =>
-  await myAxios.post("/post/set", {
-    title: title,
-    content: content,
-    description: description,
-    tags: tags,
-  });
+) => {
+  try {
+    await myAxios.post("/post/set", {
+      title: title,
+      content: content,
+      description: description,
+      tags: tags,
+    });
+    alert("포스트가 성공적으로 저장되었습니다.");
+    location.href = "/";
+  } catch (err) {
+    console.error(err);
+    alert("포스트 저장 중 오류가 발생했습니다.");
+    location.href = "/";
+  }
+};
 
 export { setPost };
